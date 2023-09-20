69adfa7808c3ef0d6e8884d2727c50f7f2e5334a
tayutaedomo <tayutaedomo@gmail.com>
2023-09-20 13:24:02 +0900

    Add links to top page

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hello World
+      <ul>
+        <li>
+          <Link href="/ethers-js">ethers.js Top</Link>
+        </li>
+        <li>
+          <Link href="/">web3.js Top (TODO)</Link>
+        </li>
+      </ul>
     </main>
   )
 }
