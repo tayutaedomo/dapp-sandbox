13659596a6b6301d5b6e9c1f0c91dc384dab6322
tayutaedomo <tayutaedomo@gmail.com>
2023-09-24 18:58:11 +0900

    Add deploy script

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
---
+++ packages/contract/scripts/deployGreeting.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const Greeting = await ethers.getContractFactory("Greeting");
+  const myName = "John";
+  const greeting = await Greeting.deploy(myName);
+
+  console.log("greeting address:", await greeting.getAddress());
+  console.log("deployer address:", deployer.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
