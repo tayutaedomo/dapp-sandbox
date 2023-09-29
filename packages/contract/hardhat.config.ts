0df5772d2ffd9170f1b0b12d035a4f5c41fe7fd0
tayutaedomo <tayutaedomo@gmail.com>
2023-09-29 21:02:06 +0900

    Add deploy setting for mumbai

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -8,6 +8,10 @@ const config: HardhatUserConfig = {
       url: process.env.SEPOLIA_URL,
       accounts: [process.env.SEPOLIA_PRIVATE_KEY || ""],
     },
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.MUMBAI_PRIVATE_KEY || ""],
+    },
   },
 };
 
